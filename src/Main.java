da59167223a7090a2f91195422d29cd41122d36a
SergeySkosyrskiy <s-s-e_96@mail.ru>
2024-02-01 21:17:31 +0800

    Исправлен System.out.println в задаче 2 и 3.

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
         System.out.println("Ф.И.О. сотрудника - " + fullName);
 
         System.out.println("Задача2");
-        System.out.println(fullName.toUpperCase());
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
         System.out.println("Задача3");
         firstName = "Семён";
@@ -23,7 +23,7 @@ public class Main {
         lastName = lastName.replace("ё", "е");*/
         fullName = lastName + " " + firstName + " " + midleName;
         fullName = fullName.replace("ё", "е");
-        System.out.println(fullName.toUpperCase());
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
 
     }
 }
